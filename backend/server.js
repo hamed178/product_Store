a5b9dcadd9fe29c9e30a8780b5f16cfc330f6502
hamed178 <hamedhussein004@gmail.com>
2026-03-13 01:37:07 +0200

    update bac

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://product-store-six-virid.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   }),
 );
 
